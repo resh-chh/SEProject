3759993d06e9749bd690060454f21c39d3c90385
rohan <rohan@localhost.localdomain>
2017-03-27 21:50:10 +0530

    Conflict fixes

--- html/counselor_page.php
+++ html/counselor_page.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" type="text/css" href="../css/counselling_page.css">
+
 <script type="text/javascript">
 	$(document).ready(function() {
 		
--- html/header.php
+++ html/header.php
@@ -2,6 +2,9 @@
 <html>
 <head>
 	<title>Career Choices</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
@@ -11,7 +14,7 @@
 	<!-- Latest compiled JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-	<link rel="stylesheet" type="text/css" href="../css/global1.css">
+	<link rel="stylesheet" type="text/css" href="../css/global.css">
 
 </head>
 <body>
